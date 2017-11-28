d4461aa9ff446adae0ff9f5fadbf134ac8966c05
Olzhas Aitkaliyev <mysyqbai@gmail.com>
2017-11-28 21:24:16 +0300

    change name

--- src/embeddings/tools/skipgram.cpp
+++ src/embeddings/tools/skipgram.cpp
@@ -477,28 +477,38 @@ void *TrainModelThread(void *id) {
           for (c = 0; c < layer1_size; c++) syn1[c + l2] += g * syn0[c + l1];
         }
         // NEGATIVE SAMPLING
-        if (negative > 0) for (d = 0; d < negative + 1; d++) {
-          if (d == 0) {
-            target = word;
-            label = 1;
-          } else {
-            next_random = next_random * (unsigned long long)25214903917 + 11;
-            target = table[(next_random >> 16) % table_size];
-            if (target == 0) target = next_random % (vocab_size - 1) + 1;
-            if (target == word) continue;
-            label = 0;
-          }
-          l2 = target * layer1_size;
-          f = 0;
-          for (c = 0; c < layer1_size; c++) f += syn0[c + l1] * syn1neg[c + l2];
-          if (f > MAX_EXP) g = (label - 1) * alpha;
-          else if (f < -MAX_EXP) g = (label - 0) * alpha;
-          else g = (label - expTable[(int)((f + MAX_EXP) * (EXP_TABLE_SIZE / MAX_EXP / 2))]) * alpha;
-          for (c = 0; c < layer1_size; c++) neu1e[c] += g * syn1neg[c + l2];
-          for (c = 0; c < layer1_size; c++) syn1neg[c + l2] += g * syn0[c + l1];
-        }
+        if (negative > 0)
+            for (d = 0; d < negative + 1; d++) {
+              if (d == 0) {
+                target = word;
+                label = 1;
+              } else {
+                next_random = next_random * (unsigned long long)25214903917 + 11;
+                target = table[(next_random >> 16) % table_size];
+                if (target == 0)
+                    target = next_random % (vocab_size - 1) + 1;
+                if (target == word)
+                    continue;
+                label = 0;
+              }
+              l2 = target * layer1_size;
+              f = 0;
+              for (c = 0; c < layer1_size; c++)
+                  f += syn0[c + l1] * syn1neg[c + l2];
+              if (f > MAX_EXP)
+                  g = (label - 1) * alpha;
+              else if (f < -MAX_EXP)
+                  g = (label - 0) * alpha;
+              else
+                  g = (label - expTable[(int)((f + MAX_EXP) * (EXP_TABLE_SIZE / MAX_EXP / 2))]) * alpha;
+              for (c = 0; c < layer1_size; c++)
+                  neu1e[c] += g * syn1neg[c + l2];
+              for (c = 0; c < layer1_size; c++)
+                  syn1neg[c + l2] += g * syn0[c + l1];
+            }
         // Learn weights input -> hidden
-        for (c = 0; c < layer1_size; c++) syn0[c + l1] += neu1e[c];
+        for (c = 0; c < layer1_size; c++)
+            syn0[c + l1] += neu1e[c];
       }
     sentence_position++;
     if (sentence_position >= sentence_length) {
